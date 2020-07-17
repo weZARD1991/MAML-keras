01d0bde926d0db47b2c023303605318ed590dce1
Runist <1004152835@qq.com>
2020-07-17 16:21:18 +0800

    fix bug

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from train import *
 from net import MAMLmodel
 import tensorflow as tf
 import config as cfg
+import os
 
 
 def main():
@@ -18,15 +19,13 @@ def main():
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
 
-    train_list = read_csv("./data/labels/train.csv")
-    valid_list = read_csv("./data/labels/val.csv")
-    test_list = read_csv("./data/labels/test.csv")
-
     maml_model = MAMLmodel(num_classes=cfg.n_way)
 
     # Step 2：一个大循环
     for epoch in range(1, cfg.epochs + 1):
         # 把数据读取放入Epoch里面，每次读出来的任务里面图片组合都不同
+        train_list = read_csv("./data/labels/train.csv")
+        valid_list = read_csv("./data/labels/val.csv")
         train_dataset = task_split(train_list)
         valid_dataset = task_split(valid_list)
 
@@ -59,7 +58,7 @@ def main():
         val_loss = []
         # valid
         for batch_id in range(valid_step):
-            batch_task = next(get_meta_batch(valid_dataset, batch_size))
+            batch_task = next(get_meta_batch(valid_dataset, cfg.batch_size))
             loss, acc = maml_train_on_batch(maml_model,
                                             batch_task,
                                             n_way=cfg.n_way,
@@ -75,6 +74,7 @@ def main():
         print("val_loss:{:.4f} val_accuracy:{:.4f}".format(np.mean(val_loss), np.mean(val_acc)))
 
     # test
+    test_list = read_csv("./data/labels/test.csv")
     test_dataset = task_split(test_list)
     # maml_eval(maml_model, test_dataset, lr_outer=cfg.inner_lr, lr_inner=cfg.outer_lr, batch_size=cfg.batch_size)
     maml_model.save_weights(cfg.save_path)
